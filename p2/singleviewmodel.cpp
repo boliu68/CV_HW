51150595053c4d3b9a7d395e993fe83a917b5715
ZHANG Runze <rzhangaj@ust.hk>
2014-03-16 10:03:48 +0800

    Complete select reference points, compute ground plane homography

--- p2/singleviewmodel.cpp
+++ p2/singleviewmodel.cpp
@@ -43,6 +43,19 @@ Vertex* Vertex::Neighbor()
 {
     return neighbor;
 }
+void Vertex::setID(int i)
+{
+    id=i;
+}
+int Vertex::ID()
+{
+    return id;
+}
+double Vertex::getDistance(const cv::Point2d &p)
+{
+    cv::Point2d d=coor2d-p;
+    return norm(d);
+}
 
 Face::Face(const vector<Vertex *> &vs)
 {
@@ -160,11 +173,108 @@ cv::Point2d SingleViewModel::getOneVanish(const vector<pair<cv::Point2d, cv::Poi
     return Denormalize(v);
 }
 
-void SingleViewModel::getVanishs(const vector<pair<cv::Point2d, cv::Point2d> > &xlines,
-                                 const vector<pair<cv::Point2d, cv::Point2d> > &ylines,
-                                 const vector<pair<cv::Point2d, cv::Point2d> > &zlines)
+cv::Vec3b SingleViewModel::getLine(const cv::Point2d &p1, const cv::Point2d &p2)
 {
-    vx=getOneVanish(xlines);
-    vy=getOneVanish(ylines);
-    vz=getOneVanish(zlines);
+    cv::Point3d hp1(p1.x,p1.y,1.0);
+    cv::Point3d hp2(p2.x,p2.y,1.0);
+    return hp1.cross(hp2);
+}
+
+cv::Point2d SingleViewModel::computeVanish(const vector<pair<cv::Point2d,cv::Point2d> > &lines,VANISH v)
+{
+    cv::POint2d vanish=getOneVanish(lines);
+    if(v==Xv)
+        vx=vanish;
+    else if(v=Yv)
+        vy=vanish;
+    else vz=vanish;
+    return vanish;
+}
+
+void SingleViewModel::initialVertex(Vertex *ver, const cv::Point2 &c2, const cv::Point &c3, Vertex::LOC loc)
+{
+    ver=new Vertex(c2);
+    ver->setCoor3d(c3);
+    ver->setLocation(Loc);
+    vertexs.push_back(ver);
+    ver->setID(vertexs.size()-1);
+}
+
+void SingleViewModel::setOrigin(const cv::Point2d &p)
+{
+    initialVertex(origin,p,cv::Point3d(0.0,0.0,0.0),Vertex::LOC::BOTTOM);
+}
+
+cv::Point2d SingleViewModel::findPointOnDirection(const cv::Point2d &p, VANISH v)
+{
+    cv::POint2d vanish;
+    if(v==Xv)
+        vanish=vx;
+    else if(v=Yv)
+        vanish=vy;
+    else vanish=vz;
+    cv::Vec3d line=getLine(origin->Coor2d(),v);
+    cv::Vec3d vl(line[1],-line[0],line[0]*p.y-line[1]*p.x);
+    cv::Vec3d np=line.cross(vl);
+    cv::Point2d np2(np[0]/np[2],np[1]/np[2]);
+    return np2;
+}
+
+void SingleViewModel::setReferencePoints(const cv::Point2d &x, const cv::Point2d &y, const cv::Point2d &z,
+                                         double xlength, double ylength, double zlength,
+                                         Vertex *xver, Vertex *yver, Vertex *zver)
+{
+    initialVertex(xver,x,cv::Point3d(xlength,0.0,0.0),Vertex::LOC::BOTTOM);
+    initialVertex(yver,y,cv::Point3d(0.0,ylength,0.0),Vertex::LOC::BOTTOM);
+    initialVertex(zver,z,cv::Point3d(0.0,0.0,zlength),Vertex::LOC::BOTTOM);
+    reference=zver;
+    reLine=getLine(origin->Coor2d(),reference->Coor2d());
+    lxy=getLine(vx,vy);
+    vector<cv::Point3d> src,dst;
+    src.push_back(origin->Coor2d());
+    src.push_back(x);
+    cv::Vec3d xline=getLine(y,vx);
+    cv::Vec3d yline=getLine(x,vy);
+    cv::Vec3d p=xline.cross(yline);
+    src.push_back(cv::Point2d(p[0]/p[2],p[1]/p[2]));
+    dst.push_back(cv::POint2d(0.0,0.0));
+    dst.push_back(cv::POint2d(xlength,0.0));
+    dst.push_back(cv::POint2d(xlength,ylength));
+    dst.push_back(cv::POint2d(0.0,ylength));
+    Homography=cv::getPerspectiveTransform(src,dst);
+    refHeight=zlength;
+}
+
+Vertex* SingleViewModel::findNearestVertex(const cv::Point2d &p)
+{
+    double m=vertexs[0]->getDistance(p);
+    int index=0;
+    for(int i=1;i<vertexs.size();i++)
+    {
+        double d=vertexs[1]->getDistance(p);
+        if(d<m)
+        {
+            m=d;
+            index=i;
+        }
+    }
+    return vertexs[index];
+}
+
+Face* SingleViewModel::findFace(const cv::Point2d &p)
+{
+    int index=-1;
+    double m=DBL_MAX;
+    for(int i=0;i<faces.size();i++)
+    {
+        Face* f=faces[i];
+        if(f->inFace(p)&&f->Area()<m)
+        {
+            m=f->Area();
+            index=i;
+        }
+    }
+    if(index==-1)
+        return NULL;
+    return faces[index];
 }
